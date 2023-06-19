f4f931e5b3b4b74c18a7c45cf2aa2888e0e892f0
MayNiklas <info@niklas-steffen.de>
2023-06-19 15:01:43 +0200

    add zsh & packages

--- configuration.nix
+++ configuration.nix
@@ -11,15 +11,30 @@
   };
 
   ### Users ###
-  users.users.root = {
-    openssh.authorizedKeys.keyFiles = [
-      (pkgs.fetchurl {
-        # replace with your own ssh key!
-        url = "https://github.com/mayniklas.keys";
-        hash = "sha256-QW7XAqj9EmdQXYEu8EU74eFWml5V0ALvbQOnjk8ce/U=";
-      })
-    ];
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users = {
+      root = {
+        openssh.authorizedKeys.keyFiles = [
+          (pkgs.fetchurl {
+            # replace with your own ssh key!
+            url = "https://github.com/mayniklas.keys";
+            hash = "sha256-QW7XAqj9EmdQXYEu8EU74eFWml5V0ALvbQOnjk8ce/U=";
+          })
+        ];
+      };
+    };
   };
+
+  ### ZSH ###
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+  };
+
+  # Needed for zsh completion of system packages, e.g. systemd
+  environment.pathsToLink = [ "/share/zsh" ];
 
   ### Locales ###
   time.timeZone = "Europe/Berlin";
@@ -29,6 +44,16 @@
     keyMap = "de";
   };
 
+  ### Packages ###
+  environment.systemPackages = with pkgs; [
+    bash-completion
+    git
+    nixpkgs-fmt
+    wget
+    zsh
+  ];
+
+  ### Nix ###
   system.stateVersion = "23.05";
 
 }
