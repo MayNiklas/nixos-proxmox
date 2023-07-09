9e5796b2c37cec9af9e5de3e195d63f016dc23c6
MayNiklas <info@niklas-steffen.de>
2023-07-09 11:37:43 +0200

    add nix settings

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,41 @@
   services.qemuGuest.enable = true;
 
   ### Nix ###
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      # this enables the technically experimental feature Flakes
+      experimental-features = nix-command flakes
+
+      # If set to true, Nix will fall back to building from source if a binary substitute fails.
+      fallback = true
+
+      # the timeout (in seconds) for establishing connections in the binary cache substituter. 
+      connect-timeout = 10
+
+      # these log lines are only shown on a failed build
+      log-lines = 25
+
+      # Free up to 1GiB whenever there is less than 512MiB left.
+      min-free = ${toString (512 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
+
+    settings = {
+      # Users allowed to run nix
+      allowed-users = [ "root" ];
+      # Save space by hardlinking store files
+      auto-optimise-store = true;
+    };
+
+    # Clean up old generations after 30 days
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
   system.stateVersion = "23.05";
 
 }
