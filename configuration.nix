f420c9a10d36582a1c382f3808a0995d4fda6540
MayNiklas <info@niklas-steffen.de>
2023-07-09 11:35:33 +0200

    add vm settings

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,39 @@
     zsh
   ];
 
-  # enable the qemu guest agent
+  ### settings specific to this VM setup ###
+  # enable after installation!
+
+  # # reduce size of the VM
+  # services.fstrim = {
+  #   enable = true;
+  #   interval = "weekly";
+  # };
+
+  # fileSystems."/" = {
+  #   device = "/dev/disk/by-label/nixos";
+  #   autoResize = true;
+  #   fsType = "ext4";
+  # };
+
+  # fileSystems."/boot" = {
+  #   device = "/dev/disk/by-label/ESP";
+  #   fsType = "vfat";
+  # };
+
+  # boot.loader.grub = {
+  #   version = 2;
+  #   device = "nodev";
+  #   efiSupport = true;
+  #   efiInstallAsRemovable = true;
+  # };
+
+  # boot.initrd.availableKernelModules = [ "9p" "9pnet_virtio" "ata_piix" "uhci_hcd" "virtio_blk" "virtio_mmio" "virtio_net" "virtio_pci" "virtio_scsi" ];
+  # boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
+  # boot.kernelModules = [ "kvm-intel" ];
+
+  # boot.growPartition = true;
+
   services.qemuGuest.enable = true;
 
   ### Nix ###
